261642540b3f3260cf73e64e90c990db5e06531c
elvira lacap <matet.lacap@gmail.com>
2011-10-25 18:48:40 +0800

    ready for production

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-#ENV['RAILS_ENV'] ||= 'production'
+ENV['RAILS_ENV'] ||= 'production'
 # Initialize the rails application
 MobileAdminBackup::Application.initialize!
 
-ActiveSupport::Dependencies.log_activity = true
+#ActiveSupport::Dependencies.log_activity = true
 
 
