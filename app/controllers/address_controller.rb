e7d7dcf100abdb263e75b003afca4c34a51c3b11
elvira lacap <matet.lacap@gmail.com>
2011-11-11 17:40:09 +0800

    Added Address controller for adding shop users delivery address/es

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,36 +1,40 @@
 class AddressController < ApplicationController
 
-  #Post method
-  #Accepts salutation_id, first_name, last_name, mobile_phone, birth_date, email address and password as input (in JSON format)
-  #Returns corresponding status and status/error message for debugging
+  #Post method for adding delivery address for shop users
+  #Accepts JSON as input
+  #Returns corresponding status and status/error message for debugging (also in JSON)
 
   def create
     
-    @DeliveryAddress  = params[:register_user]
+    @DeliveryAddress  = params[:delivery_address]
 
     begin
 
-      if @RegisterShopUser["salutation_id"] and @RegisterShopUser["first_name"] and @RegisterShopUser["last_name"] and @RegisterShopUser["email_address"] and  @RegisterShopUser["mobile_phone"] and @RegisterShopUser["password"]
+    if @DeliveryAddress
+
+      if @DeliveryAddress["shop_user_id"] and @DeliveryAddress["name"] and @DeliveryAddress["city"] and @DeliveryAddress["zip_code"] and  @DeliveryAddress["street_address01"] and @DeliveryAddress["state_id"]
 
         
-        if Salutation.find(@RegisterShopUser["salutation_id"])
+        if ShopUser.find(@DeliveryAddress["shop_user_id"]) and State.find(@DeliveryAddress["state_id"])
 
-          shop_user = ShopUser.new
-          shop_user.update_attributes(@RegisterShopUser)
-          shop_user.save!
-          message = {"status" => 0, "message" => "Registration successful", "shop_user_id" => shop_user.id}          
-          
-        else
-        
-          message = {"status" => 2, "message" => "Salutation id does not exist"}
-              
+          delivery_address = DeliveryAddress.new
+          delivery_address.update_attributes(@DeliveryAddress)
+          delivery_address.save!
+          message = {"status" => 0, "message" => "Delivery address added successfully"}
+                        
         end
         
       else
       
-        message = {"status" => 3, "message" => "Incomplete input parameters"}
+        message = {"status" => 2, "message" => "Incomplete input parameters"}
       
       end
+      
+    else
+
+      message = {"status" => 3, "message" => "No input parameter"}
+
+    end
       
     rescue ActiveRecord::RecordInvalid => e
     
